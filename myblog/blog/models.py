4a8611965f299af48fb3b99c8dfb3d306ae0cc39
maibenben <mr.mystory70@gmail.com>
2024-03-14 07:43:20 +0300

    Создание модели таблицы Пост в models.py

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    '''Данные а посте'''
+    title = models.CharField('Заголовок записи', max_length=100)
+    description = models.TextField('Текст записи')
+    author = models.CharField('Имя автора', max_length=100)
+    date = models.DateField('Дата публикации')
+
+    def __str__(self):
+        return f"{self.title}, {self.author}"
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
+
